b42e71238add8691862c4eb4a1a6bfbc459885a7
mmdghazvini666 <118440298+mmdghazvini666@users.noreply.github.com>
2023-06-17 09:02:41 +0330

    Add files via upload

--- bot.py
+++ bot.py
@@ -5,135 +5,173 @@ import requests
 
 bot = telebot.TeleBot("5729803893:AAENu5_k_0w10-rjaz2T5b8L5SPCBEEZlUk")
 admin_id = 879124022
-api = "1686920134QXERSKV7BDIFG2U"
-usernamee = "admin"
-passwordd = "killer44775566ki"
+api = "https://ger.mrkiller90.fun:8082/api&key=1686920134QXERSKV7BDIFG2U"
+matn = "🍷سلام مستر کیلر عزیز☠️"
 key1 = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True,row_width=2)
 key1.add("✍️افزودن کاربر✍️","✍️حذف کاربر✍️","🔧ادیت کاربر🔧","⚙مشخصات کاربر⚙","💾تنظیم بنر💾","🪦بکاپ🪦")
-
+keyback = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True,row_width=2)
+keyback.add("↩️برگشت↩️")
 @bot.message_handler(commands=["start"])
 def wellcome(message):
-    bot.send_message(message.chat.id, "Hi", reply_markup=key1)
+    bot.send_message(message.chat.id, matn, reply_markup=key1)
 
 @bot.message_handler()
 def info(message):
     if message.chat.id == admin_id: 
         if message.text == "✍️افزودن کاربر✍️":
-            msg = bot.send_message(message.chat.id, "🎃نام کاربر را وارد کنید :")
+            msg = bot.send_message(message.chat.id, "🎃نام کاربر را وارد کنید :",reply_markup=keyback)
             bot.register_next_step_handler(msg, name)
         elif message.text == "✍️حذف کاربر✍️":
-            mssg = bot.send_message(message.chat.id, "🎃نام کاربر را وارد کنید :")
+            mssg = bot.send_message(message.chat.id, "🎃نام کاربر را وارد کنید :",reply_markup=keyback)
             bot.register_next_step_handler(mssg, namede)
         elif message.text == "⚙مشخصات کاربر⚙":
-            mo = bot.send_message(message.chat.id, "🎃نام کاربر را وارد کنید :")
+            mo = bot.send_message(message.chat.id, "🎃نام کاربر را وارد کنید :",reply_markup=keyback)
             bot.register_next_step_handler(mo, mosh) 
         elif message.text == "💾تنظیم بنر💾":
-            bot.send_message(message.chat.id,"☕️متن خود را وارد کنید :")
+            bot.send_message(message.chat.id,"☕️متن خود را وارد کنید :",reply_markup=keyback)
             bot.register_next_step_handler(message, banner)
         elif message.text == "🪦بکاپ🪦":
             bot.register_next_step_handler(message, backup)
         elif message.text == "🔧ادیت کاربر🔧":
-            msgg = bot.send_message(message.chat.id, "🎃نام کاربر را وارد کنید :")
+            msgg = bot.send_message(message.chat.id, "🎃نام کاربر را وارد کنید :",reply_markup=keyback)
             bot.register_next_step_handler(msgg,ename)
+        elif message.text == "↩️برگشت↩️":
+            bot.send_message(message.chat.id,"↩️برگشتیم عشقم🍷",reply_markup=key1)
+        
     else:
         bot.send_message(message.chat.id, "گمشو مردک فضول👹") 
 
-        
 def name(message):
-    global namek 
-    namek = message.text
-    msg = bot.send_message(message.chat.id, "🎃رمز کاربر را وارد کنید : ")
-    bot.register_next_step_handler(msg, ramz)
+    if message.text == "↩️برگشت↩️":
+        bot.send_message(message.chat.id,"↩️برگشتیم عشقم🍷",reply_markup=key1)
+    else:
+        global namek 
+        namek = message.text
+        msg = bot.send_message(message.chat.id, "🎃رمز کاربر را وارد کنید : ",reply_markup=keyback)
+        bot.register_next_step_handler(msg, ramz)
 def ename(message):
-    global enamek 
-    enamek = message.text
-    msgg = bot.send_message(message.chat.id, "🎃رمز کاربر را وارد کنید : ")
-    bot.register_next_step_handler(msgg, eramz)
-
+    if message.text == "↩️برگشت↩️":
+        bot.send_message(message.chat.id,"↩️برگشتیم عشقم🍷",reply_markup=key1)
+    else:
+        global enamek 
+        enamek = message.text
+        msgg = bot.send_message(message.chat.id, "🎃رمز کاربر را وارد کنید : ",reply_markup=keyback)
+        bot.register_next_step_handler(msgg, eramz)
+    
 def ramz(message):
-    global ramzk
-    ramzk = message.text
-    msg = bot.send_message(message.chat.id, "🎃تعداد کاربر را وارد کنید :")
-    bot.register_next_step_handler(msg, tedad)
+    if message.text == "↩️برگشت↩️":
+        bot.send_message(message.chat.id,"↩️برگشتیم عشقم🍷",reply_markup=key1)
+    else:
+        global ramzk
+        ramzk = message.text
+        msg = bot.send_message(message.chat.id, "🎃تعداد کاربر را وارد کنید :",reply_markup=keyback)
+        bot.register_next_step_handler(msg, tedad)
 def eramz(message):
-    global eramzk
-    eramzk = message.text
-    msgg = bot.send_message(message.chat.id, "🎃تعداد کاربر را وارد کنید :")
-    bot.register_next_step_handler(msgg, etedad)
+    if message.text == "↩️برگشت↩️":
+        bot.send_message(message.chat.id,"↩️برگشتیم عشقم🍷",reply_markup=key1)
+    else:
+        global eramzk
+        eramzk = message.text
+        msgg = bot.send_message(message.chat.id, "🎃تعداد کاربر را وارد کنید :",reply_markup=keyback)
+        bot.register_next_step_handler(msgg, etedad)
 
 def tedad(message):
-    global tedadk
-    tedadk = message.text
-    msg = bot.send_message(message.chat.id, "🎃تاریخ انقضاء را وارد کنید :")
-    bot.register_next_step_handler(msg, enq)
+    if message.text == "↩️برگشت↩️":
+        bot.send_message(message.chat.id,"↩️برگشتیم عشقم🍷",reply_markup=key1)
+    else:
+        global tedadk
+        tedadk = message.text
+        msg = bot.send_message(message.chat.id, "🎃تاریخ انقضاء را وارد کنید :",reply_markup=keyback)
+        bot.register_next_step_handler(msg, enq)
 def etedad(message):
-    global etedadk
-    etedadk = message.text
-    msgg = bot.send_message(message.chat.id, "🎃تاریخ انقضاء را وارد کنید :")
-    bot.register_next_step_handler(msgg, eenq)
+    if message.text == "↩️برگشت↩️":
+        bot.send_message(message.chat.id,"↩️برگشتیم عشقم🍷",reply_markup=key1)
+    else:
+        global etedadk
+        etedadk = message.text
+        msgg = bot.send_message(message.chat.id, "🎃تاریخ انقضاء را وارد کنید :",reply_markup=keyback)
+        bot.register_next_step_handler(msgg, eenq)
 
 def enq(message):
-    global enqk
-    enqk = message.text
-    msg = bot.send_message(message.chat.id, "🎃حجم مصرفی را وارد کنید : ")
-    bot.register_next_step_handler(msg, trf)
+    if message.text == "↩️برگشت↩️":
+        bot.send_message(message.chat.id,"↩️برگشتیم عشقم🍷",reply_markup=key1)
+    else:
+        global enqk
+        enqk = message.text
+        msg = bot.send_message(message.chat.id, "🎃حجم مصرفی را وارد کنید : ",reply_markup=keyback)
+        bot.register_next_step_handler(msg, trf)
 def eenq(message):
-    global eenqk
-    eenqk = message.text
-    msgg = bot.send_message(message.chat.id, "🎃حجم مصرفی را وارد کنید : ")
-    bot.register_next_step_handler(msgg, etrf)
+    if message.text == "↩️برگشت↩️":
+        bot.send_message(message.chat.id,"↩️برگشتیم عشقم🍷",reply_markup=key1)
+    else:
+        global eenqk
+        eenqk = message.text
+        msgg = bot.send_message(message.chat.id, "🎃حجم مصرفی را وارد کنید : ",reply_markup=keyback)
+        bot.register_next_step_handler(msgg, etrf)
 
 def trf(message):
-    global trfk
-    trfk = message.text
-    url = "https://ger.mrkiller90.fun:8082/api&key="+api+"&method=adduser" 
-    etk = {
-        "username": namek,
-        "password": ramzk,
-        "multiuser": tedadk,
-        "traffic": trfk,
-        "type_traffic Required": "gb",
-        "expdate": enqk
-    }
-    requests.post(url,etk)
-    bot.send_message(message.chat.id,"☠️your user has been created✅"+"\n💥username :" " " + namek+"\n💥password :" " " + ramzk +"\n👹multiuser :" " " + tedadk+"\n🔥traffic :" " " + trfk)
+    if message.text == "↩️برگشت↩️":
+        bot.send_message(message.chat.id,"↩️برگشتیم عشقم🍷",reply_markup=key1)
+    else:
+        global trfk
+        trfk = message.text
+        url = api+"&method=adduser" 
+        etk = {
+            "username": namek,
+            "password": ramzk,
+            "multiuser": tedadk,
+            "traffic": trfk,
+            "type_traffic": "gb",
+            "expdate": enqk,
+        }
+        requests.post(url,etk)
+        bot.send_message(message.chat.id,"☠️your user has been created✅"+"\n💥username :" " " + namek+"\n💥password :" " " + ramzk +"\n👹multiuser :" " " + tedadk+"\n🔥traffic :" " " + trfk)
 def etrf(message):
-    global etrfk
-    etrfk = message.text
-    url = "https://ger.mrkiller90.fun:8082/api&key="+api+"&method=edituser" 
-    eetk = {
-        "username": enamek,
-        "password": eramzk,
-        "multiuser": etedadk,
-        "traffic": etrfk,
-        "type_traffic Required": "gb",
-        "expdate": eenqk
-    }
-    requests.post(url,eetk)
-    bot.send_message(message.chat.id,"☠️your user has been edited✅"+"\n💥username :" " " + enamek+"\n💥password :" " " + eramzk +"\n👹multiuser :" " " + etedadk+"\n🔥traffic :" " " + etrfk)
+    if message.text == "↩️برگشت↩️":
+        bot.send_message(message.chat.id,"↩️برگشتیم عشقم🍷",reply_markup=key1)
+    else:
+        global etrfk
+        etrfk = message.text
+        url = api+"&method=edituser" 
+        eetk = {
+            "username": enamek,
+            "password": eramzk,
+            "multiuser": etedadk,
+            "traffic": etrfk,
+            "type_traffic": "gb",
+            "expdate": eenqk,
+        }
+        requests.post(url,eetk)
+        bot.send_message(message.chat.id,"☠️your user has been edited✅"+"\n💥username :" " " + enamek+"\n💥password :" " " + eramzk +"\n👹multiuser :" " " + etedadk+"\n🔥traffic :" " " + etrfk)
 
 
 def namede(message):
     global delnamek
     delnamek = message.text
-    url = "https://ger.mrkiller90.fun:8082/api&key="+api+"&method=deleteuser" 
+    url = api+"&method=deleteuser" 
     result = {"username": delnamek}
     requests.post(url, result)
     bot.send_message(message.chat.id,"☠️کاربر با موفقیت حذف شد✅")
 
 def mosh(message):
-    mosh_username = message.text
-    url = f"https://ger.mrkiller90.fun:8082/api&key="+api+"&method=user&username="+mosh_username
-    response = requests.get(url)
-    data = response.json()['data'][0]
-    messagee = f"💻Username: {data['username']}\n🔗Password: {data['password']}\n🔋Traffic: {data['traffic']}\n🍷Multiuser: {data['multiuser']}\n🎈Start date: {data['startdate']}\n🎈Finish date: {data['finishdate']}"
-    bot.send_message(message.chat.id, messagee)
+    if message.text == "↩️برگشت↩️":
+        bot.send_message(message.chat.id,"↩️برگشتیم عشقم🍷",reply_markup=key1)
+    else:
+        mosh_username = message.text
+        url = api+"&method=user&username="+mosh_username
+        response = requests.get(url)
+        data = response.json()['data'][0]
+        messagee = f"💻Username: {data['username']}\n🔗Password: {data['password']}\n🔋Traffic: {data['traffic']}\n🍷Multiuser: {data['multiuser']}\n🎈Start date: {data['startdate']}\n🎈Finish date: {data['finishdate']}"
+        bot.send_message(message.chat.id, messagee)
 def banner(message):
-    matnk = message.text
-    f = open("banner.txt", "w+")
-    f.write(matnk)
-    f.close()
-    bot.send_message(message.chat.id, "☠️بنر شما با موفقیت ساخته شد !✅")
+    if message.text == "↩️برگشت↩️":
+        bot.send_message(message.chat.id,"↩️برگشتیم عشقم🍷",reply_markup=key1)
+    else:
+        matnk = message.text
+        f = open("banner.txt", "w+")
+        f.write(matnk)
+        f.close()
+        bot.send_message(message.chat.id, "☠️بنر شما با موفقیت ساخته شد !✅")
 def backup(message):
     os.system("mysqldump -u 'admin' --password='killer44775566ki' XPanel > /root/XPanel.sql")
     file_path = "/root/XPanel.sql"
